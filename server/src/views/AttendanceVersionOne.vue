d89647811ae38b1403af26bc9836d9c3d9870975
KYBee <kyb3634@naver.com>
2022-12-07 21:54:04 +0900

    :sparkles: Implement Input Structure
    Implement Input Structure

--- server/src/views/AttendanceVersionOne.vue
+++ server/src/views/AttendanceVersionOne.vue
@@ -1,11 +1,53 @@
 <template>
-  <div></div>
+  <div class="input-container">
+    <InputVueVersionOne :userInput="userInput" />
+    <button class="submit-button">만들기</button>
+  </div>
 </template>
 
 <script>
-// @ is an alias to /src
-
+import InputVueVersionOne from "@/components/InputVueVersionOne";
 export default {
-  components: {},
+  components: {
+    InputVueVersionOne,
+  },
+  data() {
+    return {
+      userInput: {
+        name: "",
+        birthday: "",
+        absentYear: "",
+        absentMonth: "",
+        absentDay: "",
+        absentTime: "0",
+        absentCategory: "0",
+        absentReason: "",
+        absentDetail: "",
+        absentPlace: "",
+        currentYear: "",
+        currentMonth: "",
+        currentDay: "",
+      },
+    };
+  },
+  methods: {
+    verifyValidation() {
+      if (
+        this.name &&
+        this.birthday &&
+        this.absentYear &&
+        this.absentMonth &&
+        this.absentDay &&
+        this.absentReason &&
+        this.absentDetail &&
+        this.absentPlace &&
+        this.currentYear &&
+        this.currentMonth &&
+        this.currentDay
+      ) {
+        alert("모든 정보를 입력해주세요.");
+      }
+    },
+  },
 };
 </script>
