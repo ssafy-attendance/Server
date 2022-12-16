f943afb15811b66d45f6cf83ab0339e5f86079d1
sxott94 <sxott94@gmail.com>
2022-12-16 17:04:49 +0900

    :memo: Add SVG file
    add svg file

--- server/src/components/PreView.vue
+++ server/src/components/PreView.vue
@@ -1,7 +1,6 @@
 <template>
-  <canvas id="container" width="600" height="800" />
+  <canvas id="container" width="793.76001" height="1122.5601" />
 </template>
-
 <script>
 export default {
   data() {
@@ -15,10 +14,10 @@ export default {
     this.context = context;
 
     const img = new Image();
-    img.src = require("@/assets/AttendVersion1_Image/출결이미지-1.png");
+    img.src = require("@/assets/AttendVersion1_Image/출결이미지.svg");
     console.log(img.src);
     img.onload = function () {
-      context.drawImage(img, 0, 0, 600, 800);
+      context.drawImage(img, 0, 0, 793.76001, 1122.5601);
     };
   },
 };
