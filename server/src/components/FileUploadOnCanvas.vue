2017bca6a124ec28a9e6f26d0b262f8991b9bac0
김동준 <dev.jun125@gmail.com>
2022-12-27 03:32:00 +0900

    :recycle: Refactor FileUploadOnCanvas Module
    Refactor FileUploadOnCanvas Module

--- server/src/components/FileUploadOnCanvas.vue
+++ server/src/components/FileUploadOnCanvas.vue
@@ -17,8 +17,7 @@
 </template>
 
 <script>
-import jsPDF from "jspdf";
-import html2canvas from "html2canvas";
+import savePdf from "@/utils/savePdf";
 export default {
   data() {
     return {
@@ -32,43 +31,56 @@ export default {
   },
   methods: {
     initCanvas() {
-      const canvas = document.querySelector("#container");
-      this.canvas = canvas;
-      const context = canvas.getContext("2d");
-      this.context = context;
-      canvas.width = window.innerWidth;
-      canvas.height = (window.innerWidth * 4) / 3;
-      this.canvasWidth = canvas.width;
-      this.canvasHeight = canvas.height;
-
-      const canvasImg2 = new Image();
-      canvasImg2.src = require("@/assets/AttendVersion1_Image/출결이미지3.svg");
-      canvasImg2.onload = function () {
-        context.drawImage(canvasImg2, 0, 0, canvas.width, canvas.height);
-      };
+      savePdf.initCanvas(this.canvas, this.context);
     },
     saveImg() {
-      html2canvas(document.querySelector("#container")).then(function (canvas) {
-        var imgData = canvas.toDataURL("image/png", 1.0);
-        var imgWidth = 210;
-        var imgHeight = (canvas.height * imgWidth) / canvas.width;
-
-        var doc = new jsPDF("p", "mm", "a4");
-
-        doc.addImage(imgData, "PNG", 0, 0, imgWidth, imgHeight);
-
-        doc.save("sample.pdf");
-      });
+      savePdf.saveImg();
     },
     uploadImg() {
-      const image = this.$refs["image"].files[0];
-      const url = URL.createObjectURL(image);
-      this.image = url;
-
-      const material = new Image();
-      material.src = this.image;
+      console.log("hi");
+      console.log(this.$refs.image.files[0]);
+      this.image = this.$refs.image.files[0];
+      this.image = savePdf.uploadImg(this.image);
       this.$emit("uploadPicture", [this.image]);
     },
+    // initCanvas() {
+    //   const canvas = document.querySelector("#container");
+    //   this.canvas = canvas;
+    //   const context = canvas.getContext("2d");
+    //   this.context = context;
+    //   canvas.width = window.innerWidth;
+    //   canvas.height = (window.innerWidth * 4) / 3;
+    //   this.canvasWidth = canvas.width;
+    //   this.canvasHeight = canvas.height;
+
+    //   const canvasImg2 = new Image();
+    //   canvasImg2.src = require("@/assets/AttendVersion1_Image/출결이미지3.svg");
+    //   canvasImg2.onload = function () {
+    //     context.drawImage(canvasImg2, 0, 0, canvas.width, canvas.height);
+    //   };
+    // },
+    // saveImg() {
+    //   html2canvas(document.querySelector("#container")).then(function (canvas) {
+    //     var imgData = canvas.toDataURL("image/png", 1.0);
+    //     var imgWidth = 210;
+    //     var imgHeight = (canvas.height * imgWidth) / canvas.width;
+
+    //     var doc = new jsPDF("p", "mm", "a4");
+
+    //     doc.addImage(imgData, "PNG", 0, 0, imgWidth, imgHeight);
+
+    //     doc.save("sample.pdf");
+    //   });
+    // },
+    // uploadImg() {
+    //   const image = this.$refs["image"].files[0];
+    //   const url = URL.createObjectURL(image);
+    //   this.image = url;
+
+    //   const material = new Image();
+    //   material.src = this.image;
+    //   this.$emit("uploadPicture", [this.image]);
+    // },
   },
 };
 </script>
---
+++ server/src/utils/savePdf.js
@@ -0,0 +1,46 @@
+import jsPDF from "jspdf";
+import html2canvas from "html2canvas";
+const initCanvas = (canvas, context) => {
+  canvas = document.querySelector("#container");
+  this.canvas = canvas;
+  context = canvas.getContext("2d");
+  this.context = context;
+  canvas.width = window.innerWidth;
+  canvas.height = (window.innerWidth * 4) / 3;
+  this.canvasWidth = canvas.width;
+  this.canvasHeight = canvas.height;
+  const canvasImg2 = new Image();
+  canvasImg2.src = require("@/assets/AttendVersion1_Image/출결이미지3.svg");
+  canvasImg2.onload = function () {
+    context.drawImage(canvasImg2, 0, 0, canvas.width, canvas.height);
+  };
+};
+
+const saveImg = () => {
+  html2canvas(document.querySelector("#container")).then(function (canvas) {
+    var imgData = canvas.toDataURL("image/png", 1.0);
+    var imgWidth = 210;
+    var imgHeight = (canvas.height * imgWidth) / canvas.width;
+
+    var doc = new jsPDF("p", "mm", "a4");
+
+    doc.addImage(imgData, "PNG", 0, 0, imgWidth, imgHeight);
+
+    doc.save("sample.pdf");
+  });
+};
+
+const uploadImg = (image) => {
+  const url = URL.createObjectURL(image);
+  image = url;
+  const material = new Image();
+  material.src = image;
+  return image;
+};
+const instance = {
+  initCanvas,
+  saveImg,
+  uploadImg,
+};
+
+export default instance;
