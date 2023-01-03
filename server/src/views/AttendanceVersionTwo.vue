7579c514721c4d44a469eaf2b42c34a3d394756d
KYBee <kyb3634@naver.com>
2023-01-04 00:37:50 +0900

    :bug: Fix Input Reason Value Error
    Fix Input Reason Value Error

--- server/src/views/AttendanceVersionTwo.vue
+++ server/src/views/AttendanceVersionTwo.vue
@@ -222,13 +222,15 @@ export default {
     },
     verifyValidation() {
       this.userInput.signatureUrl = this.canvas.toDataURL();
+
       if (
         !(
           this.userInput.campus &&
           this.userInput.class &&
           this.userInput.name &&
           this.userInput.birth &&
-          this.userInput.reason &&
+          this.userInput.reason >= 0 &&
+          this.userInput.reason < 4 &&
           this.userInput.detailReason &&
           this.userInput.signatureUrl &&
           this.attendanceDate &&
