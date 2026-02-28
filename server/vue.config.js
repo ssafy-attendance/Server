5c6d8b8b79c8e3b044e2d28152e6f076d4dfd695
ybeen.kim <ybeen.kim@samsung.com>
2026-02-28 22:54:45 +0900

    Fix Cloudflare build output directory for static deploy

--- server/vue.config.js
+++ server/vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
-  outputDir: '/var/www/html/Server/server/dist'
+  outputDir: 'dist'
 });
